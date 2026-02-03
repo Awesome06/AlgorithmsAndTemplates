e8aba5caf760d8a2e383c983355c4fd27db8658a
Mrigank Bhatnagar <47363219+Awesome06@users.noreply.github.com>
2026-02-04 02:02:55 +0530

    Add files via upload

---
+++ Default.cpp
@@ -0,0 +1,27 @@
+//USE G++ COMPILER
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long
+#define f first
+#define s second
+#define pb push_back
+#define show(x) cout << (#x) << " : " << x << endl;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+void solve();
+
+signed main() {
+  fastio();
+  int t = 1; 
+  cin >> t; // Comment if One Test Case
+  while (t--) solve();
+  return 0;
+}
+
+void solve() {
+
+}
