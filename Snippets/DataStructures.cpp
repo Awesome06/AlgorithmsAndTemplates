c135a872c82a953927da9c1d29ae070e360790d3
Mrigank Bhatnagar <47363219+Awesome06@users.noreply.github.com>
2026-02-04 02:15:50 +0530

    Add multiset and bitset examples
    Added examples of multiset and bitset in DataStructures.cpp

---
+++ Snippets/DataStructures.cpp
@@ -0,0 +1,8 @@
+//Multiset
+//Set with Repeats allowed
+multiset<int> ms;
+
+//Bitset 
+//Efficent Storage of Binary Strings, Allows Binary Operations
+bitset<10> bs(string("0010011010")); // from right to left
+bs.count() // Returns count of 1
